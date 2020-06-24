618f72336562c84faed771948dd9092fe8d7d3ff
Aleksey Radionov <radionov@surfstudio.ru>
2020-06-24 23:19:54 +0300

    small fixes and linter relax

--- lib/ui/widget/list_item/list_item_wm.dart
+++ lib/ui/widget/list_item/list_item_wm.dart
@@ -1,21 +1,20 @@
 import 'package:flutter/material.dart' hide Action;
-import 'package:injectable/injectable.dart';
-import 'package:mwwm/mwwm.dart';
-import 'package:relation/relation.dart';
-
 import 'package:gear_demo/di/di_container.dart';
 import 'package:gear_demo/domain/item.dart';
 import 'package:gear_demo/service/list/list_service.dart';
 import 'package:gear_demo/ui/common/console_message_controller.dart';
 import 'package:gear_demo/ui/common/default_error_handler.dart';
+import 'package:injectable/injectable.dart';
+import 'package:mwwm/mwwm.dart';
+import 'package:relation/relation.dart';
 
 @injectable
 class ListItemWm extends WidgetModel {
   final Item item;
   final GlobalKey<ScaffoldState> key;
 
-  final onOddTapAction = Action<void>();
-  final onEvenTapAction = Action<void>();
+  final onOddTapAction = Action();
+  final onEvenTapAction = Action();
 
   ListItemWm(
     @factoryParam this.key,
@@ -36,15 +35,16 @@ class ListItemWm extends WidgetModel {
   @override
   void onBind() {
     super.onBind();
-    subscribe<void>(onOddTapAction.stream, _onTap);
-    subscribe<void>(onEvenTapAction.stream, _onEvenTap);
+
+    subscribe(onOddTapAction.stream, (_) => _onTap());
+    subscribe(onEvenTapAction.stream, (_) => _onEvenTap());
   }
 
-  void _onTap(_) {
+  void _onTap() {
     handleError('Handle error message ${item.name}');
   }
 
-  void _onEvenTap(_) {
+  void _onEvenTap() {
     final listService = getIt.get<ListService>();
     listService.printConsole(item.name);
   }
